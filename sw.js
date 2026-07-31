d7a410d3d36dafc85ddb5a3ab792fed5084c8057
Eitan Pinczowski <eitanp2701@gmail.com>
2026-07-31 17:32:30 +0300

    dashboard snapshot 2026-08-08

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'bgu-20260808-2100';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request, {ignoreSearch: true}))
+  );
+});
